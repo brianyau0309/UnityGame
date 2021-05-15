b32fd7d02e7839465c29dee175a484d0ab67f609
Brian Yau <brianyauu@gmail.com>
2021-05-15 17:24:25 +0800

    Init

---
+++ Assets/Scripts/Gameplay/PlayerEnemyCollision.cs
@@ -0,0 +1,60 @@
+using Platformer.Core;
+using Platformer.Mechanics;
+using Platformer.Model;
+using UnityEngine;
+using static Platformer.Core.Simulation;
+
+namespace Platformer.Gameplay
+{
+
+    /// <summary>
+    /// Fired when a Player collides with an Enemy.
+    /// </summary>
+    /// <typeparam name="EnemyCollision"></typeparam>
+    public class PlayerEnemyCollision : Simulation.Event<PlayerEnemyCollision>
+    {
+        public EnemyController enemy;
+        public PlayerController player;
+
+        PlatformerModel model = Simulation.GetModel<PlatformerModel>();
+
+        public override void Execute()
+        {
+            var willHurtEnemy = player.Bounds.center.y >= enemy.Bounds.max.y;
+
+            if (willHurtEnemy)
+            {
+                var enemyHealth = enemy.GetComponent<Health>();
+                if (enemyHealth != null)
+                {
+                    enemyHealth.Decrement();
+                    if (!enemyHealth.IsAlive)
+                    {
+                        Schedule<EnemyDeath>().enemy = enemy;
+                        player.Bounce(2);
+                    }
+                    else
+                    {
+                        player.Bounce(7);
+                    }
+                }
+                else
+                {
+                    Schedule<EnemyDeath>().enemy = enemy;
+                    player.Bounce(2);
+                }
+            }
+            else
+            {
+                if (player.health.currentHP == 1) {
+                    Schedule<PlayerDeath>();
+                } else {
+                    player.health.Hurt(); // -1
+                    if (player.audioSource && player.ouchAudio)
+                        player.audioSource.PlayOneShot(player.ouchAudio);
+                    player.animator.SetTrigger("hurt");
+                }
+            }
+        }
+    }
+}
